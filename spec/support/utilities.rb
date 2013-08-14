013f68187833a3e32df1cadccfe985b3852807d3
dmitry <kosenkodmitryv@gmail.com>
2013-08-14 15:08:26 +0400

    Add integration tests for the full_title helper. Replace method 'full_title' test helper with a simple 'include' spec/support/utilities.rb

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_title = "Base Title of the Sample App on Ruby on Rails 4"
-  if page_title.empty?
-    base_title
-  else
-        "#{ base_title } | #{ page_title }"
-  end
-end
+include ApplicationHelper
